1f8075a0219fbb52935f80d6466074f1b4668a3e
karasjos <josef.karasiak@tieto.com>
2020-10-13 12:34:46 +0200

    Update deploy-lab.sh

--- deploy-lab.sh
+++ deploy-lab.sh
@@ -17,35 +17,35 @@ unset TF_VAR_AWS_account
 unset TF_VAR_host
 unset VM1_DNS
 
-DEF_ORG_ID="a3606f12-0955-4546-b768-b0472fdfdcc2"
+DEF_ORG_ID="_________________"
 #read -p "Enter your ORG ID (long format) [default=$DEF_ORG_ID]: " TF_VAR_my_org_id
 TF_VAR_my_org_id="${TF_VAR_my_org_id:-$DEF_ORG_ID}"
 #echo ".....Exporting $TF_VAR_my_org_id"
 export TF_VAR_my_org_id=$TF_VAR_my_org_id
 #echo ""
 
-DEF_TOKEN="44cyfhDg0xMoFbkh1tGYBdLxCxhJ9r2a6i931skAbbmZJeu1tpqOV1RmXmoHyScA"
+DEF_TOKEN="_________________"
 #read -p "Enter your VMC API token [default=$DEF_TOKEN]: " TF_VAR_vmc_token
 TF_VAR_vmc_token="${TF_VAR_vmc_token:-$DEF_TOKEN}"
 #echo ".....Exporting $TF_VAR_vmc_token"
 export TF_VAR_vmc_token=$TF_VAR_vmc_token
 #echo ""
 
-ACCOUNT="021027617648"
+ACCOUNT="_________________"
 #read -p "Enter your AWS Account [default=$ACCOUNT]: " TF_VAR_AWS_account
 TF_VAR_AWS_account="${TF_VAR_AWS_account:-$ACCOUNT}"
 #echo ".....Exporting $TF_VAR_AWS_account"
 export TF_VAR_AWS_account=$TF_VAR_AWS_account
 #echo ""
 
-ACCESS="AKIAQJZKX7NYICINZLHF"
+ACCESS="_________________"
 #read -p "Enter your AWS Access Key [default=$ACCESS]: " TF_VAR_access_key
 TF_VAR_access_key="${TF_VAR_access_key:-$ACCESS}"
 #echo ".....Exporting $TF_VAR_access_key"
 export AWS_ACCESS_KEY_ID=$TF_VAR_access_key
 #echo ""
 
-SECRET="QejhdlnA2SDJ7AgKx+xtIOvlk2xUo/YdjvDJ3k8E"
+SECRET="_________________"
 #read -p "Enter your AWS Secret Key [default=$SECRET]: " TF_VAR_secret_key
 TF_VAR_secret_key="${TF_VAR_secret_key:-$SECRET}"
 #echo ".....Exporting $TF_VAR_secret_key"
